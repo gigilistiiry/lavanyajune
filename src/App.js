df470286fb9ffedbbc9cc054d650f90aa7445d5c
lutfhireynaldytambora <lutfhiitamboraa@gmail.com>
2024-01-23 23:08:29 +0700

    change UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import monkey from '../src/assets/monkey.png';
+import NavBar from './components/NavBar/NavBar';
+import ImgMediaCard from './components/Cards';
 class Home extends Component {
   render() {
     return (
       <div>
+        <NavBar/>
         <div style={{
           position: 'relative',
           padding: 20
         }}>
-          <h2>LAVANYAJUNE</h2>
           <img alt='' src={monkey} style={{
             width: '150px',
             position: 'absolute',
-            top: 0,
+            top: -65,
             right: 0
-            }}/>
+            }}
+          />
+          <ImgMediaCard/>
         </div>
       </div>
     )
