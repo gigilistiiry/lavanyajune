2eac2f97f58cda998c9e7979763bf4b388e00915
lutfhireynaldytambora <lutfhiitamboraa@gmail.com>
2024-02-28 01:07:17 +0700

    clear

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import monkey from '../src/assets/monkey.png';
 class Home extends Component {
   render() {
     return (
-      <div>
-        <div style={{
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        {/* <div style={{
           position: 'relative',
           padding: 20
         }}>
@@ -15,7 +20,9 @@ class Home extends Component {
             top: 0,
             right: 0
             }}/>
-        </div>
+        </div> */}
+        <h1>GAK ADA APA-APA :)</h1>
+        <h3>hahahahaha</h3>
       </div>
     )
   }
